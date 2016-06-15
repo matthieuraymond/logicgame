8e7d11bbfe79614a46c1792d3727cdbcf3eee24f
Matthieu <matthieuraymond@hotmail.com>
2016-06-15 18:31:12 +0100

    Add rule tests

---
+++ core/src/test/RuleTest.java
@@ -0,0 +1,24 @@
+package test;
+
+import com.bob.game.inputs.Block;
+import com.bob.game.inputs.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RuleTest {
+
+    @Test
+    public void testGetLPSString() throws Exception {
+        Rule r = new Rule();
+
+        r.setRuleBlocks(new Block[] {Block.WHITE, Block.IMPLY, Block.RIGHT});
+
+        assertEquals("isIn(X,Y) & wasIn(U,V) & white(X,Y) -> goRight().", r.getString());
+    }
+
+    @Test
+    public void testIsValid() throws Exception {
+
+    }
+}
