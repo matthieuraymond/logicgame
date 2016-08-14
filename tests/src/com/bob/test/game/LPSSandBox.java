2d1ef1d2b6c312f792b743e30138a53a64924da3
Matthieu <matthieuraymond@hotmail.com>
2016-08-14 17:28:33 +0100

    Add compass

--- tests/src/com/bob/test/game/LPSSandBox.java
+++ tests/src/com/bob/test/game/LPSSandBox.java
@@ -4,14 +4,9 @@ import com.bob.game.levels.Level;
 import com.bob.game.levels.WriteLevel;
 import com.bob.game.world.LPSHandler;
 import com.bob.game.world.MapManager;
-import com.bob.lps.model.Goal;
-import com.bob.lps.model.GoalsList;
-import com.bob.lps.model.SimpleSentence;
 import com.bob.test.GdxTestRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import java.util.Set;
 
 @RunWith(GdxTestRunner.class)
 public class LPSSandBox {
@@ -22,7 +17,7 @@ public class LPSSandBox {
         MapManager m = new MapManager(lvl.getMap());
 
         LPSHandler lps = new LPSHandler(m.getLPSDescription(), "isIn(X,Y) & wasIn(U,V) & white(X,Y) -> goRight(0).isIn(X,Y) & wasIn(U,V) & white(X,Y) -> goRight(1).", lvl.getX(), lvl.getY());
-        for (int i = 0; i < 20; ++i) {
+        /*for (int i = 0; i < 20; ++i) {
             lps.update();
             GoalsList goalsList = GoalsList.getInstance();
 
@@ -34,7 +29,7 @@ public class LPSSandBox {
             }
             String s = "lol";
 
-        }
+        }*/
 
     }
 
