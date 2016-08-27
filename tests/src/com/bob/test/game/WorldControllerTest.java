50a906df7295d6b3201523e3b23492a7319abb33
matthieuraymond@hotmail.com <matthieuraymond@hotmail.com>
2016-08-27 16:13:28 +0100

    Add rule index to strategy + benchmarks in tests

--- tests/src/com/bob/test/game/WorldControllerTest.java
+++ tests/src/com/bob/test/game/WorldControllerTest.java
@@ -4,7 +4,6 @@ import com.bob.game.inputs.Block;
 import com.bob.game.inputs.InputsManager;
 import com.bob.game.levels.Level;
 import com.bob.game.levels.LevelFactory;
-import com.bob.game.levels.WriteLevel;
 import com.bob.game.world.WorldController;
 import com.bob.test.GdxTestRunner;
 import org.junit.Test;
@@ -19,24 +18,40 @@ public class WorldControllerTest {
     public void testLevels() throws Exception {
 
         LevelFactory.initialiseLevels();
+        MacroManagerTest mmt = new MacroManagerTest();
 
-        testLevel(
-                LevelFactory.WRITE.get(1),
+        testWriteLevel(
+                1,
                 new Block[][] {{Block.WHITE, Block.IMPLY, Block.RIGHT}}
         );
 
-        testLevel(
-                LevelFactory.WRITE.get(2),
+        testWriteLevel(
+                1,
+                new Block[][] {{Block.WHITE, Block.IMPLY, Block.RIGHT}}
+        );
+
+        testWriteLevel(
+                1,
+                new Block[][] {{Block.WHITE, Block.IMPLY, Block.RIGHT}}
+        );
+
+        testWriteLevel(
+                1,
+                new Block[][] {{Block.WHITE, Block.IMPLY, Block.RIGHT}}
+        );
+
+        testWriteLevel(
+                2,
                 new Block[][] {{Block.WHITE, Block.IMPLY, Block.RIGHT},{Block.RED, Block.IMPLY, Block.DOWN}}
         );
 
-        testLevel(
-                LevelFactory.WRITE.get(3),
+        testWriteLevel(
+                3,
                 new Block[][] {{Block.NOT, Block.WHITE, Block.IMPLY, Block.RIGHT}}
         );
 
-        testLevel(
-                LevelFactory.WRITE.get(4),
+        testWriteLevel(
+                4,
                 new Block[][] {
                         {Block.WHITE, Block.AND, Block.WHITE_PREV, Block.IMPLY, Block.RIGHT},
                         {Block.RED, Block.IMPLY, Block.DOWN},
@@ -46,23 +61,75 @@ public class WorldControllerTest {
                 }
         );
 
+        testReadLevel(0);
+        testReadLevel(1);
+        testReadLevel(2);
+
+        mmt.testLevel1();
+        mmt.testLevel2();
+
     }
 
-    public void testLevel(Level lvl, Block[][] rules) {
+    public void testWriteLevel(int lvlIndex, Block[][] rules) {
 
-        WorldController wc = new WorldController();
-        wc.setupWorld(lvl);
-
+        Level lvl = LevelFactory.WRITE.get(lvlIndex);
 
         InputsManager im = new InputsManager();
         im.resetRules(rules);
 
+        Long start = System.nanoTime();
+
+        WorldController wc = new WorldController();
+        wc.setupWorld(lvl);
         wc.startLPSAnimation(lvl, im.getRulesString());
-        for (int i=0; i < 100; i++) {
+
+
+        Long settedUp = System.nanoTime();
+
+        int i;
+        for (i=0; i < 100; i++) {
             wc.updateWorld(1f);
             if (wc.isLevelWon()) break;
         }
 
+        Long done = System.nanoTime();
+
+        System.out.println("Write level " +lvlIndex + ": " + i + " cycles in: "
+                + Long.toString((done - settedUp)/1000) + " us, set-up of: "
+                + Long.toString((settedUp - start)/1000) + " us, total: "
+                + Long.toString((done - start)/1000) + " us");
         assertTrue(wc.isLevelWon());
+    }
+
+    public void testReadLevel(int lvlIndex) {
+
+        Level lvl = LevelFactory.READ.get(lvlIndex);
+
+        InputsManager im = new InputsManager();
+        im.resetRules(lvl.getRules());
+
+        Long start = System.nanoTime();
+
+        WorldController wc = new WorldController();
+        wc.setupWorld(lvl);
+        wc.startLPSAnimation(lvl, im.getRulesString());
+
+
+        Long settedUp = System.nanoTime();
+
+        int i;
+        for (i=0; i < 200; i++) {
+            wc.updateWorld(1f);
+            if (wc.isLevelWon() || wc.isOnQuestionMark()) break;
+        }
+
+        Long done = System.nanoTime();
+
+        System.out.println("Read level " + (lvlIndex+1) + ": " + i + " cycles in: "
+                + Long.toString((done - settedUp)/1000) + " us, set-up of: "
+                + Long.toString((settedUp - start)/1000) + " us, total: "
+                + Long.toString((done - start)/1000) + " us");
+
+        assertTrue(wc.isLevelWon() || wc.isOnQuestionMark());
     }
 }
